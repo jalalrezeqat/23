163e0da9fc4b45d140b75b546458c1371005fdb3
jalalrezeqat <jlal.osama.rezeqta@gmail.com>
2022-03-19 19:07:29 +0200

    sssw

---
+++ admindash/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit inventore, ipsa nemo repellendus expedita aut dignissimos libero necessitatibus assumenda velit natus quasi optio autem quam. Hic blanditiis distinctio eum optio!</h1>
+    <h2>Lorem ipsum dolor sit amet consecteturds adipisicing elit. Ea totam nihil rem assumenda reprehenderit voluptatem, ratione velit perferendis deleniti odit expedita necessitatibus ipsum quam quia, eos cum nam, vero nulla!</h2>
+</body>
+</html>
