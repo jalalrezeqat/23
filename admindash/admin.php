55a483c2c8287cb13517e3110b5fecb3a8929c5c
jalalrezeqat <jlal.osama.rezeqta@gmail.com>
2022-03-19 19:00:25 +0200

    lorem

---
+++ admindash/admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit inventore, ipsa nemo repellendus expedita aut dignissimos libero necessitatibus assumenda velit natus quasi optio autem quam. Hic blanditiis distinctio eum optio!</h1>
+</body>
+</html>
